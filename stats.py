a6db5b47080a84b5db14d0d83f60fa96f2f286c8
Serhii Mohylevskyi <serhii.mohylevskyi.y@nixs.com>
2023-10-25 23:18:23 +0200

    Refactoring (a sprinkle of chatGPT magic)

--- stats.py
+++ stats.py
@@ -1,25 +1,22 @@
 import json
 from utils import *
 from datetime import datetime
+from collections import OrderedDict
 
 
 def parse_data(history_file_path):
     with open(history_file_path, encoding="utf8") as f:
-        history = f.read()
-    json_history = json.loads(history)
-    return json_history['messages']
+        return json.loads(f.read())['messages']
 
 
 def get_character_count_top(parsed_data, top_count=9999):
     message_counts = {}
 
     for message in parsed_data:
-        if message['type'] != 'service' and isinstance(message['text'], str) and message['text'] != '':
+        if message['type'] != 'service' and isinstance(message['text'], str) and message['text']:
             author = message['from']
-            if author in message_counts:
-                message_counts[author] += len(message['text'])
-            else:
-                message_counts[author] = len(message['text'])
+            message_counts[author] = message_counts.get(author, 0) + len(message['text'])
+
     sorted_data = dict(sorted(message_counts.items(), key=lambda item: item[1], reverse=True))
     return take_from_dict(sorted_data, top_count)
 
@@ -28,12 +25,10 @@ def get_message_count_top(parsed_data, top_count=conf.REALLY_BIG_NUMBER):
     message_counts = {}
 
     for message in parsed_data:
-        if message['type'] != 'service' and isinstance(message['text'], str) and message['text'] != '':
+        if message['type'] != 'service' and isinstance(message['text'], str) and message['text']:
             author = message['from']
-            if author in message_counts:
-                message_counts[author] += 1
-            else:
-                message_counts[author] = 1
+            message_counts[author] = message_counts.get(author, 0) + 1
+
     sorted_data = dict(sorted(message_counts.items(), key=lambda item: item[1], reverse=True))
     return take_from_dict(sorted_data, top_count)
 
@@ -41,14 +36,12 @@ def get_message_count_top(parsed_data, top_count=conf.REALLY_BIG_NUMBER):
 def get_average_characters_per_message(parsed_data, top_count=conf.TOP_CHARACTER_PER_MESSAGE, lowest=False):
     top_messages = get_message_count_top(parsed_data)
     top_characters = get_character_count_top(parsed_data, 25)
-    for entry in top_characters:
-        top_characters[entry] = int(top_characters[entry]/top_messages[entry])
-    if not lowest:
-        sorted_data = dict(sorted(top_characters.items(), key=lambda item: item[1], reverse=True))
-        return take_from_dict(sorted_data, top_count)
-    else:
-        sorted_data = dict(sorted(top_characters.items(), key=lambda item: item[1], reverse=False))
-        return take_from_dict(sorted_data, top_count)
+
+    for entry, count in top_characters.items():
+        top_characters[entry] = int(count / top_messages.get(entry, 1))
+
+    sorted_data = dict(sorted(top_characters.items(), key=lambda item: item[1], reverse=not lowest))
+    return take_from_dict(sorted_data, top_count)
 
 
 def get_top_weekdays(parsed_data, top_count=conf.TOP_WEEKDAYS_COUNT):
@@ -57,10 +50,8 @@ def get_top_weekdays(parsed_data, top_count=conf.TOP_WEEKDAYS_COUNT):
     for message in parsed_data:
         if message['type'] != 'service':
             weekday = datetime.strptime(message['date'], conf.DATE_FULL_FORMAT).strftime(conf.DATE_WEEKDAY_READABLE_FORMAT)
-            if weekday in message_counts:
-                message_counts[weekday] += 1
-            else:
-                message_counts[weekday] = 1
+            message_counts[weekday] = message_counts.get(weekday, 0) + 1
+
     sorted_data = dict(sorted(message_counts.items(), key=lambda item: item[1], reverse=True))
     return take_from_dict(sorted_data, top_count)
 
@@ -71,10 +62,8 @@ def get_message_count_per_date(parsed_data):
     for message in parsed_data:
         if message['type'] != 'service':
             date = datetime.strptime(message['date'], conf.DATE_FULL_FORMAT).strftime(conf.DATE_SHORT_FORMAT)
-            if date in message_counts:
-                message_counts[date] += 1
-            else:
-                message_counts[date] = 1
+            message_counts[date] = message_counts.get(date, 0) + 1
+
     return message_counts
 
 
@@ -84,11 +73,12 @@ def get_message_count_per_hour(parsed_data):
     for message in parsed_data:
         if message['type'] != 'service':
             hour = datetime.strptime(message['date'], conf.DATE_FULL_FORMAT).strftime(conf.DATE_HOUR_FORMAT)
-            if hour in message_counts:
-                message_counts[hour] += 1
-            else:
-                message_counts[hour] = 1
-    return dict(sorted(message_counts.items(), key=lambda item: item[0], reverse=False))
+            message_counts[hour] = message_counts.get(hour, 0) + 1
+
+    sorted_dict = OrderedDict(sorted(message_counts.items()))
+    sorted_dict.move_to_end('00', last=True)
+
+    return sorted_dict
 
 
 def get_top_day_streak(parsed_data, top_people=conf.TOP_STREAK_PEOPLE_COUNT, top_streaks=conf.TOP_STREAK_STREAKS_COUNT):
--- utils.py
+++ utils.py
@@ -5,31 +5,28 @@ import config as conf
 
 def find_top_ranges(active_ranges, top_people, top_streaks):
     result = {}
-    for person in active_ranges:
-        if active_ranges[person] != {}:
-            sorted_data = dict(sorted(active_ranges[person].items(), key=lambda item: item[1], reverse=True))
-            result[person] = take_from_dict(sorted_data, top_streaks)
+
+    for person, data in active_ranges.items():
+        if data:
+            sorted_data = dict(sorted(data.items(), key=lambda item: item[1], reverse=True))
+            result[person] = dict(list(sorted_data.items())[:top_streaks])
+
     result = dict(sorted(result.items(), key=lambda item: list(item[1].items())[0][1], reverse=True))
-    result = take_from_dict(result, top_people)
-    return result
+    return take_from_dict(result, top_people)
 
 
 def get_message_dates_for_person(messages, person):
-    person_dates_of_messages = []
-    for message in messages[person]:
-        person_dates_of_messages.append(
-            datetime.strptime(message['date'], conf.DATE_FULL_FORMAT).strftime(conf.DATE_SHORT_FORMAT))
-    return person_dates_of_messages
+    return [
+        datetime.strptime(message['date'], conf.DATE_FULL_FORMAT).strftime(conf.DATE_SHORT_FORMAT)
+        for message in messages[person]
+    ]
 
 
 def generate_date_range(start_date, end_date):
     date_earliest = datetime.strptime(start_date, conf.DATE_FULL_FORMAT)
     date_latest = datetime.strptime(end_date, conf.DATE_FULL_FORMAT)
     delta = date_latest - date_earliest
-    date_range = []
-    for i in range(delta.days + 1):
-        date_range.append(date_earliest + timedelta(days=i))
-
+    date_range = [date_earliest + timedelta(days=i) for i in range(delta.days + 1)]
     return date_range
 
 
